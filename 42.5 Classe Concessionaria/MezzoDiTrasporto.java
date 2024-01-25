6f4d95de53e8b67229dffac8fafe20b7c9492176
FullMetalPie <andrea.pietri06@gmail.com>
2024-01-25 22:21:00 +0100

    Completed

--- 42.5 Classe Concessionaria/MezzoDiTrasporto.java
+++ 42.5 Classe Concessionaria/MezzoDiTrasporto.java
@@ -1,4 +1,32 @@
-abstract class MezzoDiTrasporto {
-    private String marca = "undefined";
-    
+public abstract class MezzoDiTrasporto {
+    private String marca;
+    private String modello;
+    private float velocita; 
+    private float potenza;
+    private int numeroRuote;
+    private String tipoMotore;
+    private float litri100km;
+
+    public MezzoDiTrasporto() {
+        marca = "undefined";
+        modello = "undefined";
+        velocita = 0; 
+        potenza = 0;
+        numeroRuote = 0;
+        tipoMotore = "undefined";
+        litri100km = 0;
+    }
+
+    public MezzoDiTrasporto(String brand, String model, float speed, float power, int nWheels, String engineType, float liters100k) {
+        marca = brand;
+        modello = model;
+        velocita = speed; 
+        potenza = power;
+        numeroRuote = nWheels;
+        tipoMotore = engineType;
+        litri100km = liters100k;
+    }
+
+    public abstract float calcoloCarburante();
+    public abstract float calcoloPotenzaRuota();
 }
