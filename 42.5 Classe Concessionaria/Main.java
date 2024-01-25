340e00d2767319338c8c332bd706f000c53b17f9
FullMetalPie <andrea.pietri06@gmail.com>
2024-01-25 21:55:38 +0100

    Created main file

---
+++ 42.5 Classe Concessionaria/Main.java
@@ -0,0 +1,36 @@
+/*
+Esercizio Java - OOP - Concessionaria
+Con delle classi e delle classi astratte, modellare al meglio le seguenti entità:
+- mezzo di trasporto
+- veicolo (a motore)
+- automobile
+- motociclo
+
+All’interno delle entità (decidere in modo opportuno in quali) vanno inseriti i seguenti
+attributi:
+- marca
+- modello
+- velocità (media)
+- potenza
+- quantità di ruote motrici
+- tipo di motore (benzina, diesel)
+- litri di carburante consumato per percorrere 100 km (per un percorso misto a velocità media)
+
+All’interno delle entità (decidere in modo opportuno in quali) vanno inseriti i seguenti metodi:
+- calcola costo del carburante al km
+- calcola la potenza per singola ruota motrice
+
+Scrivere un main che:
+- crei un oggetto per ogni tipo di classe concreta
+- chieda e faccia inserire all’utente i prezzi al litro di benzina e gasolio
+- chieda all’utente il modello e poi stampi a standard output: i dati del relativo oggetto, il
+costo del carburante per ogni km percorso, la potenza per singola ruota motrice
+
+Pietri Andrea 4AI
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
