92b484dd19129e00b247569407b312388c2a6805
FullMetalPie <andrea.pietri06@gmail.com>
2024-01-26 14:26:35 +0100

    Updated main file

--- Es39 Classe Vagone-VagonePasseggeri-VagoneMerci/Main.java
+++ Es39 Classe Vagone-VagonePasseggeri-VagoneMerci/Main.java
@@ -28,6 +28,8 @@ Pietri Andrea 4AI
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao");
+        Treno t = new Treno();
+
+        t.stampa();
     }
 }
