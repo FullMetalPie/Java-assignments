d7b12679ee75041dbe4da814223d72558ad464bf
FullMetalPie <andrea.pietri06@gmail.com>
2024-01-29 19:39:15 +0100

    Created main file

---
+++ Es02/Main.java
@@ -0,0 +1,14 @@
+/*Un numero di Krampus è un numero naturale per cui nelle prime quattro cifre della parte
+frazionaria della propria radice quarta si alternano cifre pari e cifre dispari, o viceversa (PDPD
+oppure DPDP, con P = pari e D = dispari).
+
+Pietri Andrea 4AI
+*/
+
+package Es02;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
